823e1c3b39e8d7ca5094a073dcf3442f99760940
Orion Musselman <orionmm@pm.me>
2022-06-03 11:00:04 -0400

    Now LISTEN here BUDDY If I hear one more seg fault out of you I'm going to create a fault right on this keyboard of yours. Kapesh?

--- isometricer/iso.c
+++ isometricer/iso.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <math.h>
+#include <string.h>
 
 #define BUFFER_SIZE 512
 
@@ -58,6 +59,7 @@ bool isTree(int *graph, int len, int vertices) {
 
 // Output allocated size is assumed to be BUFFER_SIZE
 // Input assumed to be a tree
+// len must be divisable by two and > 0
 void treeCodeList(int *tree, int len, int vertices, int *output, int *outputLen) {
     typedef struct {
         int path;
@@ -128,7 +130,7 @@ void treeCodeList(int *tree, int len, int vertices, int *output, int *outputLen)
     }
 }
 
-int treeCode(int *tree, int len, int vertices) {
+unsigned long treeCode(int *tree, int len, int vertices, int *error) {
     int codes[BUFFER_SIZE];
     int codesLen;
     // https://oeis.org/A000040
@@ -140,21 +142,23 @@ int treeCode(int *tree, int len, int vertices) {
         263,269,271
     };
     int counts[sizeof(primes) / sizeof(int)] = {0};
-    int numPrimes = sizeof(primes) / sizeof(int);
-    int result = 0;
+    const int numPrimes = sizeof(primes) / sizeof(int);
+    unsigned long result = 1;
 
     treeCodeList(tree, len, vertices, codes, &codesLen);
     for (int i = 0; i < codesLen; i++) {
         int c = codes[i];
         if (c >= numPrimes) {
             printf("Buddy that's breaking the prime gap\n");
+            if (error != NULL)
+                *error = 1;
             return -1;
         }
         counts[c]++;
     }
     for (int i = 0; i < numPrimes; i++) {
         if (counts[i] != 0) {
-            result += pow(primes[i], counts[i]);
+            result *= pow(primes[i], counts[i]);
         }
     }
 
@@ -163,10 +167,87 @@ int treeCode(int *tree, int len, int vertices) {
 
 bool isIsometricTree(int *graphA, int lenA, int verticesA, int *graphB, int lenB, int verticesB) {
     return lenA == lenB && verticesA == verticesB &&
-        treeCode(graphA, lenA, verticesA) == treeCode(graphB, lenB, verticesB);
+        treeCode(graphA, lenA, verticesA, NULL) == treeCode(graphB, lenB, verticesB, NULL);
+}
+
+// Output is a 2-d matrix of BUFFER_SIZE x BUFFER_SIZE
+// Vertices >= 2
+void bruteTestVertices(int vertices, int *output, int *outputLen, int *seriesLenOutput) {
+    // The current tested series
+    int series[BUFFER_SIZE] = {0};
+    // List of non-isomorphic series
+    const int seriesLen = (vertices - 1) * 2;
+    const size_t outputStride = BUFFER_SIZE;
+
+    *outputLen = 0;
+    *seriesLenOutput = seriesLen;
+
+    while (series[seriesLen - 1] < vertices) {
+        if (isTree(series, seriesLen, vertices)) {
+            bool good = true;
+            for (int i = 0; i < *outputLen; i++) {
+                if (isIsometricTree(output + i * outputStride, seriesLen, vertices, series, seriesLen, vertices)) {
+                    good = false;
+                    break;
+                }
+            }
+            if (good) {
+                memcpy(output + *outputLen * outputStride, series, sizeof(int) * seriesLen);
+                (*outputLen)++;
+            }
+
+        }
+        series[0] += 1;
+        // Do not iterate over the last series item, since that is checked
+        // as the exit condition for the wrapper loop
+        for (int i = 0; i < seriesLen - 1 && series[i] >= vertices; i++) {
+            series[i] = 0;
+            series[i+1]++;
+        }
+    }
+}
+
+// Func is a function like bruteTestVertices()
+// tests is assumed to be size of size endVertex - startVertex
+void validateTests(void (*func)(int,int*,int*,int*), int startVertex, int endVertex, bool *tests) {
+    // https://oeis.org/A000055
+    // Index directly corresponds to vertex (start is vertex zero)
+    const long answers[] = {
+            1,1,1,1,2,3,6,11,23,47,106,235,551,1301,3159,
+            7741,19320,48629,123867,317955,823065,2144505,
+            5623756,14828074,39299897,104636890,279793450,
+            751065460,2023443032,5469566585,14830871802,
+            40330829030,109972410221,300628862480,
+            823779631721,2262366343746,6226306037178
+    };
+    const int numAnswers = sizeof(answers) / sizeof(long);
+    int answerable = 0;
+
+    int series[BUFFER_SIZE][BUFFER_SIZE];
+    int seriesLen;
+    int indvSeriesLen;
+
+    // To prevent buffer overflow, only take what I can...
+    for (; answerable < numAnswers && answers[answerable] * 2 < BUFFER_SIZE; answerable++);
+
+    answerable = answerable < endVertex ? answerable : endVertex;
+
+    for (int i = startVertex; i < answerable; i++) {
+        bruteTestVertices(i, (int *) series, &seriesLen, &indvSeriesLen);
+        printf("seriesLen = %i\n", seriesLen);
+        for (int i = 0; i < seriesLen; i++) {
+            printf("[");
+            for (int s = 0; s < indvSeriesLen; s++) {
+                printf("%i,", series[i][s]);
+            }
+            printf("]\n");
+        }
+        tests[i] = seriesLen == answers[i];
+    }
 }
 
 int main(int argc, char **argv) {
+    /*
     int testGraph[] = {
         3, 7, 3, 9, 3, 2, 2, 0, 2, 1, 4, 10, 4, 11, 11, 12, 12, 3
     };
@@ -180,6 +261,32 @@ int main(int argc, char **argv) {
         printf("%i,", codeList[i]);
     }
     printf("]\n");
+    */
+
+    /*
+    int series[BUFFER_SIZE][BUFFER_SIZE];
+    int seriesLen;
+    int indvSeriesLen;
+    bruteTestVertices(4, (int *) series, &seriesLen, &indvSeriesLen);
+    for (int i = 0; i < seriesLen; i++) {
+        printf("[");
+        for (int s = 0; s < indvSeriesLen; s++) {
+            printf("%i,", series[i][s]);
+        }
+        printf("]\n");
+    }
+    */
+
+
+    const int start = 2;
+    const int end = 5;
+    const int numResults = end - start;
+    bool results[numResults];
+    validateTests(bruteTestVertices, start, end, results);
+    for (int i = start; i < end; i++) {
+        printf("i=%i -> %s\n", i, results[i] ? "PASS" : "FAIL");
+    }
+    
     return 0;
 }
 
