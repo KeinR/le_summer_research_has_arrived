55147b1df23b0c33f80dcee482f1c4ea7a00ea0b
Orion Musselman <orionmm@pm.me>
2022-06-08 10:58:38 -0400

    ioewgjoegjoiwegji

--- centrism/centrism.c
+++ centrism/centrism.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
 #define BUFFER_SIZE 512
 
 void treeJordanCenter(int *tree, int len, int *outA, int *outB) {
     // Worst time complexity of O(len * 5/2)
+    // IE, this is O(n) in terms of the size of the tree
     
     typedef struct {
         int value;
@@ -70,7 +73,84 @@ void treeJordanCenter(int *tree, int len, int *outA, int *outB) {
             leavesLen = newLeavesLen;
         }
     }
+}
 
+// Approximately O(n^2), which is, if nothing else, better than Floyd–Warshall
+void graphJordanCenter(int *graph, int len, int *output, int *outputLen) {
+    typedef struct {
+        int data[BUFFER_SIZE];
+        int len;
+    } bufferType;
+    
+    // Adacency map
+    bufferType vertexMap[BUFFER_SIZE] = {0};
+    // List of all unique vertices
+    bufferType vertices;
+    // Alternating vertex buffers
+    bufferType vertexBuffers[2];
+    bool getMap[BUFFER_SIZE] = {0};
+    
+    int bestScore = INT_MAX;
+    // Uncecessary, does nothing, but still good practice I think.
+    *outputLen = 0;
+
+    // O(edges)
+    for (int i = 0; i < len/2; i++) {
+        // Could compress this by using modulo
+        // to get the other one
+        int a = graph[i*2];
+        int b = graph[i*2+1];
+        bufferType *av = &vertexMap[a];
+        bufferType *bv = &vertexMap[b];
+        av->data[av->len++] = b;
+        bv->data[bv->len++] = a;
+        if (av->len == 1) {
+            vertices.data[vertices.len++] = a;
+        }
+        if (bv->len == 1) {
+            vertices.data[vertices.len++] = a;
+        }
+    }
+
+    // O(vertices^2)
+    for (int i = 0; i < vertices.len; i++) {
+        int v = vertices.data[i];
+        int score = 0;
+        int ab = 0;
+        int ib = 1;
+        vertexBuffers[ab].data[0] = v;
+        vertexBuffers[ab].len = 1;
+
+        // Can get rid of this entirely if we convert
+        // getMap to an integer map and give each loop
+        // a unique integer value to represent a GET
+        memset(getMap, 0, sizeof(getMap));
+
+        do {
+            score++;
+            vertexBuffers[ib].len = 0;
+            for (int f = 0; f < vertexBuffers[ab].len; f++) {
+                int vert = vertexBuffers[ab].data[f];
+                if (!getMap[vert]) {
+                    getMap[vert] = true;
+                    memcpy(vertexBuffers[ib].data + vertexBuffers[ib].len, vertexMap[vert].data, sizeof(int) * vertexMap[vert].len);
+                    vertexBuffers[ib].len += vertexMap[vert].len;
+                }
+            }
+
+            int oab = ab;
+            ab = ib;
+            ib = oab;
+        } while (vertexBuffers[ab].len > 0);
+
+        if (score < bestScore) {
+            bestScore = score;
+            *outputLen = 0;
+        }
+        if (score == bestScore) {
+            output[(*outputLen)++] = v;
+        }
+    }
 }
 
 
